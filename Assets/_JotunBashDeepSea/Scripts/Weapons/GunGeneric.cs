f60b2fb79e3b5f83e1762f6dd7845afa8cb07499
sayronaught <sayro_naught@hotmail.com>
2023-08-07 14:20:04 +0200

    Gun reload

--- Assets/_JotunBashDeepSea/Scripts/Weapons/GunGeneric.cs
+++ Assets/_JotunBashDeepSea/Scripts/Weapons/GunGeneric.cs
@@ -7,11 +7,6 @@ using UnityEngine.XR;
 
 public class GunGeneric : MonoBehaviour
 {
-    public UnityEngine.XR.InputDevice leftController;
-    public UnityEngine.XR.InputDevice rightController;
-    public float updateControllerTimer = 0f;
-    public bool leftTrigger = false;
-    public bool rightTrigger = false;
 
     public GameObject prefabBullet;
     public Transform muzzleLocation;
@@ -30,10 +25,6 @@ public class GunGeneric : MonoBehaviour
     public float reloadDelay = 1f;
 
     private GameController mainGC;
-    private XRGrabInteractable myXrGrab;
-
-    private XRDirectInteractor interactor = null;
-    public bool IsGrabbing = false;
 
     public void eventFocus()
     {
@@ -57,24 +48,10 @@ public class GunGeneric : MonoBehaviour
         fireGun();
     }
 
-    private void updateController()
-    {  
-        /*
-        if (Application.isEditor) return;
-        var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
-        var rightHandDevices = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
-        leftController = leftHandDevices[0];
-        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
-        rightController = rightHandDevices[0];
-        */
-    }
-
     // Start is called before the first frame update
     void Start()
     {
         mainGC = GameObject.Find("GameController").GetComponent<GameController>();
-        myXrGrab = GetComponent<XRGrabInteractable>();
     }
 
     public void fireGun()
@@ -106,17 +83,6 @@ public class GunGeneric : MonoBehaviour
     void Update()
     {
         fireDelay -= Time.deltaTime;
-        /*
-        if (!beingHeld) return;
-        leftController.IsPressed(InputHelpers.Button.Trigger, out leftTrigger);
-        rightController.IsPressed(InputHelpers.Button.Trigger, out rightTrigger);
-        if (rightTrigger && fireDelay < 0f ) fireGun();
-        if (transform.forward.y < -.75f && Bullets < clipsize ) reloadGun();
-        fireDelay -= Time.deltaTime;
-        updateControllerTimer -= Time.deltaTime;
-        if (!(updateControllerTimer < 0f)) return;
-        */
-        updateController();
-        updateControllerTimer = 1f;
+        if (transform.forward.y < -.75f && Bullets < clipsize) reloadGun();
     }
 }
