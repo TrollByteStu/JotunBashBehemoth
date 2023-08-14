f6f96704f1215ea3f006de5e09b53a826113c82e
sayronaught <sayro_naught@hotmail.com>
2023-08-14 08:37:02 +0200

    distance sorted list of working baits

--- Assets/_JotunBashDeepSea/Scripts/GameController/GameController.cs
+++ Assets/_JotunBashDeepSea/Scripts/GameController/GameController.cs
@@ -11,6 +11,29 @@ public class GameController : MonoBehaviour
 
     public List<Bait> activeBait;
 
+    public Bait checkForBaits(GameObject ownPrefab, Transform ownTransform)
+    {
+        if (activeBait.Count > 0)
+        {
+            List<Bait> workingBaits = new List<Bait>();
+            foreach( Bait testBait in activeBait)
+            {
+                if (testBait.doesThisBaitWorkOnMe(ownPrefab)) workingBaits.Add(testBait);
+            }
+            if ( workingBaits.Count > 0)
+            {
+                workingBaits.Sort((bait1, bait2) =>
+                {
+                    float distance1 = (bait1.transform.position - ownTransform.position).sqrMagnitude;
+                    float distance2 = (bait2.transform.position - ownTransform.position).sqrMagnitude;
+                    return distance1.CompareTo(distance2);
+                });
+                return workingBaits[0];
+            } 
+        }
+        return null;
+    }
+
     private void Awake()
     {
         _instance = this;
