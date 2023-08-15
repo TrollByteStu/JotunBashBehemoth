bdbec62b51e29d310cf888e4f47da1eb97bd1fa7
myjak <patrick@myjak.dk>
2023-08-15 13:53:31 +0200

    GlassBottle

---
+++ Assets/_JotunBashDeepSea/Scripts/Props/GlassBottle.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassBottle : MonoBehaviour
+{
+    public GameObject wholeBottle;
+    public GameObject fractureedBottle;
+
+    private Rigidbody myRigidBody;
+
+
+    public void eventSelect()
+    {
+
+    }
+
+    public void eventUnselect()
+    {
+        myRigidBody.isKinematic = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Ammo")
+        {
+            wholeBottle.SetActive(false);
+            fractureedBottle.SetActive(true);
+            Destroy(gameObject, 5f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
