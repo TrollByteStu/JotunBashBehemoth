d3a5f526783451f0fb443d011611ce3dc38774a5
sayronaught <sayro_naught@hotmail.com>
2023-08-18 14:23:22 +0200

    Barrels drifting in wind and react when hitting raft

--- Assets/_JotunBashDeepSea/Scripts/Hookable/Barrel.cs
+++ Assets/_JotunBashDeepSea/Scripts/Hookable/Barrel.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
+    public GameObject AddInventoryPrefab;
+    public int AddInventoryAmount = 10;
+
+    private GameController mainGC;
+    private Rigidbody myRigidBody;
+
+    private WindZone mainWind;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ( collision.transform.tag == "Raft")
+        { // this has been picked up, add to inventory
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainGC = GameObject.Find("GameController").GetComponent<GameController>();
+        myRigidBody = GetComponent<Rigidbody>();
+        mainWind = mainGC.gcWeather.mainWind;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // bobbing around in the wind
+        myRigidBody.AddForce(mainWind.transform.forward * (mainWind.windMain * 0.1f) + Random.insideUnitSphere * mainWind.windTurbulence, ForceMode.Force);
     }
 }
