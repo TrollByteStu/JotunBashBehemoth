a7114f4884bdf7d928455c9904df22d92bbc8c54
infernerner <101186811+infernerner@users.noreply.github.com>
2023-08-22 12:28:46 +0200

    gannet handler

---
+++ Assets/_JotunBashDeepSea/Scripts/Actors/Gannet/GannetHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GannetHandler : MonoBehaviour
+{
+    float _LastSpawnTime;
+    int _StartSpawn;
+    public int _MaxGannets;
+    List<GameObject> _Gannets;
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
