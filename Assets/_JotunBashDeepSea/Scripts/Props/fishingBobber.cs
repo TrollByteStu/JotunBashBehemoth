668afd1f336211f5065c2c553f2db97888383134
sayronaught <sayro_naught@hotmail.com>
2023-08-25 15:26:33 +0200

    fishing bobber setup

---
+++ Assets/_JotunBashDeepSea/Scripts/Props/fishingBobber.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fishingBobber : MonoBehaviour
+{
+    public enum states { hanging,flying, bobbing, fishing }
+    public states currentState = states.hanging;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
