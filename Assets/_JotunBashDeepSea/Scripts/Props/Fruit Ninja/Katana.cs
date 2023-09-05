81a999d36bb4cd3b74f2bf7f343170a3d445765c
infernerner <101186811+infernerner@users.noreply.github.com>
2023-09-05 11:27:16 +0200

    me big dumb bad at time

--- Assets/_JotunBashDeepSea/Scripts/Props/Fruit Ninja/Katana.cs
+++ Assets/_JotunBashDeepSea/Scripts/Props/Fruit Ninja/Katana.cs
@@ -7,7 +7,7 @@ public class Katana : MonoBehaviour
     public bool _BeingHeld = false;
     public bool _Touched = false;
 
-    float _CurrentTimeScale;
+    public float _CurrentTimeScale = 1;
     public float _MinTimeScale;
     public bool _SlowTime;
     private Rigidbody _Rigidbody;
@@ -17,11 +17,28 @@ public class Katana : MonoBehaviour
         _Rigidbody = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        if (_SlowTime  && _BeingHeld)
+        {
+            _CurrentTimeScale -= Time.unscaledDeltaTime / 2;
+            _CurrentTimeScale = Mathf.Clamp(_CurrentTimeScale, _MinTimeScale, 1);
+            Time.timeScale = _CurrentTimeScale;
+        }
+        else if (_BeingHeld)
+        {
+            _CurrentTimeScale += Time.unscaledDeltaTime / 2;
+            _CurrentTimeScale = Mathf.Clamp(_CurrentTimeScale, _MinTimeScale, 1);
+            Time.timeScale = _CurrentTimeScale;
+        }
+    }
 
     public void eventSelect()
     {
         _BeingHeld = true;
         _Touched = true;
+        if (_SlowTime)   
+            _SlowTime = false;
     }
 
     public void eventDeselect()
@@ -29,17 +46,17 @@ public class Katana : MonoBehaviour
         _BeingHeld = false;
         _Rigidbody.isKinematic = false;
         transform.SetParent(null);
-
+        if (_SlowTime)
+            _SlowTime = false;
     }
 
     public void eventActivate()
     {
-        // time scale stuff
-        Time.timeScale = _MinTimeScale;
+        _SlowTime = true;
     }
 
     public void eventDeactivate()
     {
-        Time.timeScale = 1;
+        _SlowTime = false;
     }
 }
