6b90d880a60b56908051272c71368f8dfb963426
sayronaught <sayro_naught@hotmail.com>
2023-09-10 13:54:27 +0200

    Harpoon stuck in enemy sound activated

--- Assets/_JotunBashDeepSea/Scripts/Props/spear.cs
+++ Assets/_JotunBashDeepSea/Scripts/Props/spear.cs
@@ -12,9 +12,12 @@ public class spear : MonoBehaviour
     public bool _Stuck = false;
     private float _DestroyTime;
 
+    private AudioSource _MyAS;
+
     private void Start()
     {
         myRB = GetComponent<Rigidbody>();
+        _MyAS = GetComponent<AudioSource>();
     }
 
     private void FixedUpdate()
@@ -55,6 +58,7 @@ public class spear : MonoBehaviour
                 _StuckInEnemy = true;
                 transform.SetParent(collision.transform);
                 myRB.isKinematic = true;
+                _MyAS.Play();
             }
             else if (collision.gameObject.GetComponentInParent<Gannet>())
             {
