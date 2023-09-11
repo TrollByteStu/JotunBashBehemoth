f3e0556b13ceddc131e2e0ae14fb282b593e87de
sayronaught <sayro_naught@hotmail.com>
2023-09-11 12:43:07 +0200

    shark dinner UI turns on

--- Assets/_JotunBashDeepSea/Scripts/Actors/Sharks/circlingShark.cs
+++ Assets/_JotunBashDeepSea/Scripts/Actors/Sharks/circlingShark.cs
@@ -73,6 +73,7 @@ public class circlingShark : InfBadMath
             {
                 _Bait.transform.position = _Bait.transform.parent.position;
                 _Bait.transform.rotation = _Bait.transform.parent.rotation;
+                GameObject.Find("SharkDinner").SetActive(true);
             } else {
                 Destroy(_Bait.gameObject);
                 _Angle = AngleOnCircle(transform.position.x, transform.position.z, Vector3.Distance(_BoatRig.transform.position, transform.position));
