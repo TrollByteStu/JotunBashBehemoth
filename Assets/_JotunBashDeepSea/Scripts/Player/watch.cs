62cdd2b242786312ed0710d4051784156bd76f99
infernerner <101186811+infernerner@users.noreply.github.com>
2023-09-12 11:18:12 +0200

    dual katana and watch timmer

--- Assets/_JotunBashDeepSea/Scripts/Player/watch.cs
+++ Assets/_JotunBashDeepSea/Scripts/Player/watch.cs
@@ -7,16 +7,21 @@ public class watch : MonoBehaviour
 {
     public TMP_Text watchText;
     public Material watchGUI;
+    GameController GameController;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameController = GameController.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var timeInSecondsInt = (int)GameController.secondsLeft;
+        var minutes = timeInSecondsInt / 60;  //Get total minutes
+        var seconds = timeInSecondsInt - (minutes * 60);  //Get seconds for display alongside minutes
+        watchText.text = minutes.ToString("D2") + ":" + seconds.ToString("D2");
+        watchGUI.SetFloat("Vector1_4", ((GameController.secondsLeft / GameController.timeLimit) - 0.5f));
     }
 }
--- Assets/_JotunBashDeepSea/Scripts/Props/Fruit Ninja/Katana.cs
+++ Assets/_JotunBashDeepSea/Scripts/Props/Fruit Ninja/Katana.cs
@@ -7,42 +7,45 @@ public class Katana : MonoBehaviour
     public bool _BeingHeld = false;
     public bool _Touched = false;
 
+    private bool _SlowTime;
     private float _DeleteTime;
     private Rigidbody _Rigidbody;
     private GameControllerFruitNinja _GcFruitNinja;
+    private AudioSource _AudioSource;
 
     private void Start()
     {
         _Rigidbody = GetComponent<Rigidbody>();
+        _AudioSource = GetComponent<AudioSource>();
         _GcFruitNinja = GameController.Instance.gcFruitNinja;
-        _GcFruitNinja._Katana = gameObject;
     }
 
     private void Update()
     {
         if (_DeleteTime + 10 < Time.time && !_BeingHeld && _Touched)
         {
-            _GcFruitNinja._On = false;
-            _GcFruitNinja._Stage = 0;
+            _GcFruitNinja._Katana.Remove(gameObject);
             Destroy(gameObject);
         }
 
         if (transform.position.y <= -10)
         {
-            _GcFruitNinja._On = false;
-            _GcFruitNinja._Stage = 0;
+            _GcFruitNinja._Katana.Remove(gameObject);
             Destroy(gameObject);
         }
     }
      
     public void eventSelect()
     {
-        _GcFruitNinja._On = true;
-        _GcFruitNinja._Stage = 0;
+        if (!_Touched)
+        {
+            _GcFruitNinja._Katana.Add(gameObject);
+            _AudioSource.Play();
+            _GcFruitNinja._On = true;
+            _GcFruitNinja._Stage = 0;
+        }
         _BeingHeld = true;
         _Touched = true;
-        if (_GcFruitNinja._SlowTime)
-            _GcFruitNinja._SlowTime = false;
     }
 
     public void eventDeselect()
@@ -51,17 +54,22 @@ public class Katana : MonoBehaviour
         _BeingHeld = false;
         _Rigidbody.isKinematic = false;
         transform.SetParent(null);
-        if (_GcFruitNinja._SlowTime)
-            _GcFruitNinja._SlowTime = false;
+        if (_SlowTime)
+        {
+            _GcFruitNinja._SlowTimeBool -= 1;
+            _SlowTime = false;
+        }
     }
 
     public void eventActivate()
     {
-        _GcFruitNinja._SlowTime = true;
+        _SlowTime = true;
+        _GcFruitNinja._SlowTimeBool += 1;
     }
 
     public void eventDeactivate()
     {
-        _GcFruitNinja._SlowTime = false;
+        _SlowTime = false;
+        _GcFruitNinja._SlowTimeBool -= 1;
     }
 }
