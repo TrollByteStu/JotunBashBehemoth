40299c8dcbde0d73f21d52e65cba550b2c6cce6e
sayronaught <sayro_naught@hotmail.com>
2023-09-18 08:43:28 +0200

    Sharklunch UI finished

--- Assets/_JotunBashDeepSea/Scripts/UI/SharkDinner.cs
+++ Assets/_JotunBashDeepSea/Scripts/UI/SharkDinner.cs
@@ -6,19 +6,41 @@ public class SharkDinner : MonoBehaviour
 {
     private float timeLeft;
     private CanvasGroup myCG;
+    private AudioSource myAS;
+
+    void resetUI()
+    {
+        timeLeft = 5f;
+        myCG.alpha = 1f;
+        myAS.Stop();
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        transform.GetChild(Random.Range(0, transform.childCount)).gameObject.SetActive(true);
+    }
 
     // Start is called before the first frame update
     void Awake()
     {
         myCG = GetComponent<CanvasGroup>();
-        timeLeft = 2f;
+        myAS = GetComponent<AudioSource>();
+        resetUI();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timeLeft > 0f) timeLeft -= Time.deltaTime;
+        if (timeLeft > 0f)
+        {
+            timeLeft -= Time.deltaTime;
+            if ( !myAS.isPlaying) myAS.Play();
+        }
         if (timeLeft <= 0f && myCG.alpha > 0f) myCG.alpha -= Time.deltaTime;
-        if (timeLeft <= 0f && myCG.alpha <= 0f) gameObject.SetActive(false);
+        if (timeLeft <= 0f && myCG.alpha <= 0f)
+        {
+            resetUI();
+            gameObject.SetActive(false);
+        }
     }
 }
